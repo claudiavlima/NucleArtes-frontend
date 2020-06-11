eabde6ec3e27c1df403edc6166d450115db34a62
Claudia Lima <limaclaudia@hotmail.com>
2020-06-11 12:26:20 -0300

    Fix error category

--- src/redux/actions/categorieActions.js
+++ src/redux/actions/categorieActions.js
@@ -1,13 +1,6 @@
 import {
-    FETCH_CATEGORIES,
-    ADD_CATEGORIE_PENDING,
-    ADD_CATEGORIE_SUCCESS,
-    ADD_CATEGORIE_ERROR,
-    DELETE_CATEGORIE_PENDING,
-    DELETE_CATEGORIE_SUCCESS,
-    DELETE_CATEGORIE_ERROR
+    FETCH_CATEGORIES
 } from './types'
-import store from '../store'
 
 // FETCH ACTIONS
 export const fetchCategories = () => dispatch => {
