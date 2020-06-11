716d7ada37ec6aaf7344fa4c7218c81312426c85
Claudia Lima <limaclaudia@hotmail.com>
2020-06-11 11:28:50 -0300

    Fetch products reducer

---
+++ src/redux/reducers/productReducer.js
@@ -0,0 +1,38 @@
+import {
+    FETCH_PRODUCTS,
+    ADD_PRODUCT_PENDING,
+    ADD_PRODUCT_SUCCESS,
+    ADD_PRODUCT_ERROR,
+    UPDATE_PRODUCT_PENDING,
+    UPDATE_PRODUCT_SUCCESS,
+    UPDATE_PRODUCT_ERROR,
+    DELETE_PRODUCT_PENDING,
+    DELETE_PRODUCT_SUCCESS,
+    DELETE_PRODUCT_ERROR,
+    SET_SELECTED_PRODUCT_ID,
+    ORDER_PRODUCTS_BY_PRICE,
+    SET_SELECTED_CATEGORY
+ } from '../actions/types'
+
+const initialState = {
+    items: [],
+    filteredItems: [],
+    error: null,
+    isLoading: false,
+    message: undefined,
+    title:'',
+    categoryName:'TODOS'
+}
+
+export default function ( state = initialState, action) {
+    switch(action.type) {
+        case FETCH_PRODUCTS:
+        return {
+            ...state,
+            items: action.payload,
+            filteredItems: action.payload
+        }
+        default:
+        return state
+    }
+}
