5150f664bb8d368ae8692982c0f17cdc009f5024
Claudia Lima <limaclaudia@hotmail.com>
2020-06-16 13:04:22 -0300

    Add login in combineReducer

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux'
 import productReducer from './productReducer'
 import categoryReducer from './categorieReducer'
+import loginReducer from './loginReducer'
 
 export default combineReducers({
     products: productReducer,
-    categories: categoryReducer
+    categories: categoryReducer,
+    users: loginReducer
 })
